1ce08cb1d0c4de24da8971859e372754ba435057
huskar666 <148646923+huskar666@users.noreply.github.com>
2025-07-09 21:59:35 +0300

    code

--- script.js
+++ script.js
@@ -9,3 +9,28 @@
       btn.classList.add('active');
     });
   });
+
+  const cardsPerPage = 20;
+  let currentPage = 0;
+  const countryCode = 'US';
+
+  async function fetchEvents(page = 0) {
+    const API_URL = `https://app.ticketmaster.com/discovery/v2/events.json?limit=${cardsPerPage}&page=${page}&countryCode=${countryCode}&segmentName=Film&apikey=49FrD8b5pF7reRwv5Ebt667wyQ9AQQPZ`
+  }
+const card = document.querySelector('.main__container--div--card');
+const modal = document.getElementById('myModal');
+const closeBtn = document.querySelector('.modal__close');
+
+card.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
